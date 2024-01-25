d33bc2bdde8f9f3cab502238dbbd7491c0bf0838
andrewmchorse <andrew.mchorse@duke.edu>
2024-01-25 16:28:11 -0500

    adding if statements *for real this time*

--- mat_gx_to_nii.py
+++ mat_gx_to_nii.py
@@ -11,9 +11,21 @@ def mat_gx_to_nii(mat_path, data_path):
      
     matfile = loadmat(mat_path)
 
+    subject_id = mat_path[-15:-10]
+
+    # List of OS subjects with old .mat file name conventions
+    prerefact_os = ['OS001', 'OS002', 'OS003', 'OS005', 'OS006', 'OS007', 'OS008', 'OS009', 'OS010', 'OS011', 'OS012', 'OS013', 'OS014', 'OS015', 'OS016', 'OS017', 'OS018', 'OS019', 'OS020', 'OS021', 'OS022', 'OS023', 'OS024']
+
     # EVERYTHING GX GAS
     gx_vent_path = data_path + 'gx_vent_norm.nii'
-    gx_vent = np.absolute(matfile['image_gas_highreso']) #pre-refactor name: 'gx_vent_highreso'
+
+    if subject_id == 'OS001': 
+        gx_vent = np.absolute(matfile['vent_highreso_cor'])
+    elif subject_id in prerefact_os: 
+        gx_vent = np.absolute(matfile['gx_vent_highreso'])
+    else:
+        gx_vent = np.absolute(matfile['image_gas_highreso'])
+
     gx_vent_norm = (gx_vent - np.amin(gx_vent))/np.amax(gx_vent)
 
     # Put it in the correct orientation
@@ -33,7 +45,13 @@ def mat_gx_to_nii(mat_path, data_path):
 
     # Ventilation binning
     gx_vent_bin_path = data_path + 'gx_vent_bin.nii'
-    gx_vent_bin = np.absolute(matfile['image_gas_binned']) # pre-refactor name: 'gx_vent_highreso_binning'
+
+    if subject_id == 'OS001': 
+        gx_vent_bin = np.absolute(matfile['vent_highreso_binning'])
+    elif subject_id in prerefact_os: 
+        gx_vent_bin = np.absolute(matfile['gx_vent_highreso_binning'])
+    else:
+        gx_vent_bin = np.absolute(matfile['image_gas_binned'])
 
     # Put it in the correct orientation
     gx_vent_bin_transpose = np.transpose(gx_vent_bin, (1, 0, 2))
@@ -75,7 +93,11 @@ def mat_gx_to_nii(mat_path, data_path):
     # Data
 
     membrane_path = data_path + 'mem.nii'
-    membrane = np.absolute(matfile['image_membrane2gas']) #pre-refactor name: 'bar2gas'
+
+    if subject_id in prerefact_os:
+        membrane = np.absolute(matfile['bar2gas'])
+    else:
+        membrane = np.absolute(matfile['image_membrane2gas'])
 
     # Put it in the correct orientation
     membrane_transpose = np.transpose(membrane, (1, 0, 2))
@@ -93,7 +115,11 @@ def mat_gx_to_nii(mat_path, data_path):
 
     # Membrane Binning
     membrane_bin_path = data_path + 'mem_bin.nii'
-    membrane_bin = np.absolute(matfile['image_membrane2gas_binned']) #pre-refactor 'bar2gas_binning' 
+
+    if subject_id in prerefact_os:
+        membrane_bin = np.absolute(matfile['bar2gas_binning'])
+    else:
+        membrane_bin = np.absolute(matfile['image_membrane2gas_binned'])
 
     # Put it in the correct orientation
     membrane_bin_transpose = np.transpose(membrane_bin, (1, 0, 2))
@@ -114,7 +140,11 @@ def mat_gx_to_nii(mat_path, data_path):
     # EVERYTHING RBC 
     # Data
     rbc_path = data_path + 'rbc.nii'
-    rbc = np.absolute(matfile['image_rbc2gas']) #pre-refactor name: 'rbc2gas'
+
+    if subject_id in prerefact_os:
+        rbc = np.absolute(matfile['rbc2gas'])
+    else:
+        rbc = np.absolute(matfile['image_rbc2gas'])
 
     # Put it in the correct orientation
     rbc_transpose = np.transpose(rbc, (1, 0, 2))
@@ -132,7 +162,11 @@ def mat_gx_to_nii(mat_path, data_path):
 
     # Bin
     rbc_bin_path = data_path + 'rbc_bin.nii'
-    rbc_bin = np.absolute(matfile['image_rbc2gas_binned']) #pre-refactor name:'rbc2gas_binning'
+
+    if subject_id in prerefact_os:
+        rbc_bin = np.absolute(matfile['rbc2gas_binning'])
+    else:
+        rbc_bin = np.absolute(matfile['image_rbc2gas_binned'])
 
     # Put it in the correct orientation
     rbc_bin_transpose = np.transpose(rbc_bin, (1, 0, 2))
